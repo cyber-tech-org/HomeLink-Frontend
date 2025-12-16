04a90a613c8e4815280ae7160ab4ce98019b74b5
David Ezekiel <titusdavid39@gmail.com>
2025-12-16 12:48:15 +0100

    Initial commit
    Generated by create-expo-app 3.5.3.

---
+++ src/store/index.ts
@@ -0,0 +1,17 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { useDispatch, useSelector, TypedUseSelectorHook } from 'react-redux';
+
+// Example slice reducer (we'll add real ones later)
+const rootReducer = {
+    // auth: authReducer,
+};
+
+export const store = configureStore({
+    reducer: rootReducer,
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+export const useAppDispatch: () => AppDispatch = useDispatch;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
